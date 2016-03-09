e6a7e1b8483aa13847d7251df1b985168fcb4b27
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2016-03-09 13:20:13 +0100

    Updated frontend

--- tools/vagrant/setup.sh
+++ tools/vagrant/setup.sh
@@ -3,7 +3,7 @@
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 apt-get -q -y install nginx sqlite python python-dev python-pip moreutils nodejs
 
-npm i -g bower
+npm i -g bower phantomjs
 
 make -C /var/www install-backend
 su vagrant -c 'make -C /var/www install-frontend'
