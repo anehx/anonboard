b8f8c54f5e2c2f44ec1188132fd70a393b6f633d
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2016-04-12 08:39:55 +0200

    Removed vagrant

--- tools/vagrant/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-apt-get -q -y install nginx sqlite python python-dev python-pip moreutils nodejs
-
-npm i -g bower phantomjs
-
-make -C /var/www install-backend
-su vagrant -c 'make -C /var/www install-frontend'
